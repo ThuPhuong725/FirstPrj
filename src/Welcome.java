fdcdb1fc23410585f824bd3827dfd85816a37e43
phuong <23696981.phuong@student.iuh.edu.vn>
2024-08-15 14:57:12 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Vu Ngoc Thu Phuong!");
+        System.out .println("Welcome to th DHKTPM19A!");
+    }
+}
